f77bf1f086166d3d4780b613569122141aaa769a
Lucas Amaral <amarall.tx@gmail.com>
2024-01-16 20:10:28 -0300

    Ver 1.0 HTML/CSS Main screen

---
+++ index.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pokémon Team Builder</title>
+    <link rel="stylesheet" href="styles/main.css">
+</head>
+<body>
+    <div class="container">
+        <header class="header">
+            <img class="github-logo" src="assets/images/github-mark-white.png" alt="" srcset="">
+            <div class="account-btns">
+                <input type="button" class="signin-btn" value="Sign In">
+                <input type="button" class="signup-btn" value="Sign Up">
+            </div>
+        </header>
+        <div class="main-content">
+            <main class="main-right">
+                
+                    <div class="main-right-top">
+                        <input type="text" name="pkm-identifier" class="pkm-identifier" 
+                        id="pkm-identifier"
+                        placeholder="Enter a pokémon name or number...">
+                        <!-- <img src="assets/images/save-icon.svg" alt="" srcset="" class="save-icon"> -->
+                    </div>
+                
+                    <div class="team-card">
+                        <div class="pkm-card">
+                            <div class="pkm-sprite-box">
+                                <img src="./assets/images/pokeball-bg.svg" alt="" class="pkm-sprite" id="pkm-img-1">
+                            </div>
+                            <div class="pkm-details"
+                            id="pkm-details-1">
+                                <div class="pkm-details-left">
+                                   <img src="assets/type_icons/fire.svg"  id="pkm-details-1-left-type" class="pkm-type-fire" alt="" srcset="">
+                                </div>
+                                <div class="pkm-details-mid">
+                                    <div id="pkm-details-name-1" class="pkm-details-name">
+                                        
+                                    </div>
+                                    <div id="pkm-details-number-1" class="pkm-details-number">
+                                        
+                                    </div>
+                                </div>
+                                <div class="pkm-details-right">
+                                    <img src="assets/type_icons/psychic.svg" id="pkm-details-1-right-type" class="pkm-type-psychic" alt="" srcset="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="pkm-card">
+                            <div class="pkm-sprite-box">
+                                <img src="assets/images/ditto.png" alt="" class="pkm-sprite" id="pkm-img-2">
+                            </div>
+                            <div class="pkm-details"
+                            id="pkm-details-2">
+                                <div class="pkm-details-left">
+                                   <img src="assets/type_icons/fire.svg" id="pkm-details-2-left-type" class="pkm-type-fire" alt="" srcset="">
+                                </div>
+                                <div class="pkm-details-mid">
+                                    <div id="pkm-details-name-2" class="pkm-details-name">
+                                        
+                                    </div>
+                                    <div id="pkm-details-number-2" class="pkm-details-number">
+                                        
+                                    </div>
+                                </div>
+                                <div class="pkm-details-right">
+                                    <img src="assets/type_icons/psychic.svg" id="pkm-details-2-right-type" class="pkm-type-psychic" alt="" srcset="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="pkm-card">
+                            <div class="pkm-sprite-box">
+                                <img src="assets/images/ditto.png" alt="" class="pkm-sprite" id="pkm-img-3">
+                            </div>
+                            <div class="pkm-details"
+                            id="pkm-details-3">
+                                <div class="pkm-details-left">
+                                   <img src="assets/type_icons/fire.svg" id="pkm-details-3-left-type" class="pkm-type-fire" alt="" srcset="">
+                                </div>
+                                <div class="pkm-details-mid">
+                                    <div id="pkm-details-name-3" class="pkm-details-name">
+                                        
+                                    </div>
+                                    <div id="pkm-details-number-3" class="pkm-details-number">
+                                        
+                                    </div>
+                                </div>
+                                <div class="pkm-details-right">
+                                    <img src="assets/type_icons/psychic.svg" id="pkm-details-3-right-type" class="pkm-type-psychic" alt="" srcset="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="pkm-card">
+                            <div class="pkm-sprite-box">
+                                <img src="assets/images/ditto.png" alt="" class="pkm-sprite" id="pkm-img-4">
+                            </div>
+                            <div class="pkm-details"
+                            id="pkm-details-4">
+                                <div class="pkm-details-left">
+                                   <img src="assets/type_icons/fire.svg" id="pkm-details-4-left-type" class="pkm-type-fire" alt="" srcset="">
+                                </div>
+                                <div class="pkm-details-mid">
+                                    <div id="pkm-details-name-4" class="pkm-details-name">
+                                        
+                                    </div>
+                                    <div id="pkm-details-number-4" class="pkm-details-number">
+                                        
+                                    </div>
+                                </div>
+                                <div class="pkm-details-right">
+                                    <img src="assets/type_icons/psychic.svg" id="pkm-details-4-right-type" class="pkm-type-psychic" alt="" srcset="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="pkm-card">
+                            <div class="pkm-sprite-box">
+                                <img src="assets/images/ditto.png" alt="" class="pkm-sprite" id="pkm-img-5">
+                            </div>
+                            <div class="pkm-details"
+                            id="pkm-details-5">
+                                <div class="pkm-details-left">
+                                   <img src="assets/type_icons/fire.svg" id="pkm-details-5-left-type" class="pkm-type-fire" alt="" srcset="">
+                                </div>
+                                <div class="pkm-details-mid">
+                                    <div id="pkm-details-name-5" class="pkm-details-name">
+                                        
+                                    </div>
+                                    <div id="pkm-details-number-5" class="pkm-details-number">
+                                        
+                                    </div>
+                                </div>
+                                <div class="pkm-details-right">
+                                    <img src="assets/type_icons/psychic.svg" id="pkm-details-5-right-type" class="pkm-type-psychic" alt="" srcset="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="pkm-card">
+                            <div class="pkm-sprite-box">
+                                <img src="assets/images/ditto.png" alt="" class="pkm-sprite" id="pkm-img-6">
+                            </div>
+                            <div class="pkm-details"
+                            id="pkm-details-6">
+                                <div class="pkm-details-left">
+                                   <img src="assets/type_icons/fire.svg" id="pkm-details-6-left-type" class="pkm-type-fire" alt="" srcset="">
+                                </div>
+                                <div class="pkm-details-mid">
+                                    <div id="pkm-details-name-6" class="pkm-details-name">
+                                        
+                                    </div>
+                                    <div id="pkm-details-number-6" class="pkm-details-number">
+                                        
+                                    </div>
+                                </div>
+                                <div class="pkm-details-right">
+                                    <img src="assets/type_icons/psychic.svg" id="pkm-details-6-right-type" class="pkm-type-psychic" alt="" srcset="">
+                                </div>
+                            </div>
+                        </div>
+                    
+                    </div>
+    
+            </main>
+            <aside class="main-left">
+            </aside>
+        </div>
+        <footer class="footer">
+            <a href="http://github.com/amarall.tx"></a>
+        </footer>
+    </div>
+
+    <script src="script/get_pkm.js"></script>
+</body>
+</html>
